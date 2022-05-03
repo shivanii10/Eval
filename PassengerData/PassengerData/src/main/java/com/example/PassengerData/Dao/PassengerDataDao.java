da4b7381547ce00b0dce41090871f4aa2afd141f
Shivani Surana <shivanisurana10@gmail.com>
2022-05-03 10:13:38 +0530

    Delete PassengerData/PassengerData directory

--- PassengerData/PassengerData/src/main/java/com/example/PassengerData/Dao/PassengerDataDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.PassengerData.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.PassengerData.Model.Passenger_Details;
-
-public interface PassengerDataDao extends JpaRepository<Passenger_Details,Integer>{
-
-}
